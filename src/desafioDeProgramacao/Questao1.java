08bd626712a1ec1411aeb06cac3495cdce7fad40
barbara figueiroa <bfamorim@outlook.com>
2022-02-16 18:24:38 -0300

    Desafio Capgemini

---
+++ src/desafioDeProgramacao/Questao1.java
@@ -0,0 +1,34 @@
+package desafioDeProgramacao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Questao1 {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		List<String> degraus = new ArrayList<>();
+
+		 
+
+		System.out.print("Digite a quantidade de vezes: ");
+		int numDegraus = leia.nextInt();
+
+		 
+
+		for (int i = 0; i < numDegraus; i++) {
+		degraus.add(" ".repeat(numDegraus - i) + "*".repeat(i + 1));
+		}
+
+		 
+
+		for (String d : degraus ) {
+		System.out.println(d);
+		}
+		
+		leia.close();
+
+	}
+
+}
---
+++ src/desafioDeProgramacao/Questao2.java
@@ -0,0 +1,68 @@
+package desafioDeProgramacao;
+
+import java.util.Scanner;
+
+
+public class Questao2 {
+	
+	public static void main(String[] args){
+	Scanner leia = new Scanner(System.in);
+		
+		boolean validaNumero = false;
+	    boolean validaMaiuscula = false;
+	    boolean validaMinuscula = false;
+	    boolean validaSimbolo = false;
+	    
+	    
+	    System.out.println("|_______REQUISITOS PARA CRIAÇÃO DE SENHA_________|"+ "\n" +
+	            "|* Possua no mínimo 6 caracteres_________________|"+ "\n" +
+	            "|* Contenha no mínimo 1 digito___________________|"+ "\n" +
+	            "|* Contenha no mínimo 1 letra em minúsculo_______|" + "\n" +
+	            "|* Contenha no mínimo 1 letra em maiúsculo_______|" + "\n" +
+	            "|* Contenha no mínimo 1 caractere especial: _____|" + "\n" + "|(!@#$%^&*()-+)__________________________________|" + "\n");
+	    System.out.print("Crie a sua senha: ");
+		String senha1 = leia.next();
+		
+		if (senha1.length() < 6) {
+			
+			System.out.print("Sua senha não atende aos requisitos de segurança com apenas " + (senha1.length()) + " caracteres. É necessário conter no minimo 6. Quantidade de caracteres que falta(m): " + (6 - senha1.length() + " caractere(s) \n"));
+		}
+		else {
+			for (char c : senha1.toCharArray()) {
+		         if (c >= '0' && c <= '9') {
+		        	 validaNumero = true;
+		         } else if (c >= 'A' && c <= 'Z') {
+		        	 validaMaiuscula = true;
+		         } else if (c >= 'a' && c <= 'z') {
+		        	 validaMinuscula = true;
+		         } else {
+		        	 validaSimbolo = true;
+		         }
+		    }	    
+		}
+		
+		if(!validaNumero) {
+			System.out.println("Coloque um número na sua senha!");
+		}
+		if(!validaMaiuscula) {
+			System.out.println("Coloque pelo menos uma letra Maiuscula!");
+		}
+		if(!validaMinuscula) {
+			System.out.println("Coloque pelo menos uma letra Minuscula!");
+		}
+		if (!validaSimbolo) {
+			System.out.println("Coloque pelo menos um caracter especial");
+		}
+		
+		if(validaNumero && validaMaiuscula && validaMinuscula && validaSimbolo) {
+			System.out.println("Senha validada com sucesso!");
+		}
+		
+		
+		leia.close();
+	}
+
+}
+
+
+
